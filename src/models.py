d543dd34bb50fb2a2f62e3767fe4abbf717ec58b
AEGiner <andresesquivel2011@gmail.com>
2025-12-18 02:39:48 +0000

    Added intagram tables

--- src/models.py
+++ src/models.py
@@ -1,19 +1,52 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, Integer, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    __tablename__ = 'user'
+    user_id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(
+        String(30), unique=True, nullable=False)
+    first_name: Mapped[str] = mapped_column(String(20), nullable=False)
+    last_name: Mapped[str] = mapped_column(String(20), nullable=False)
+    email: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    posts: Mapped[list['Post']] = relationship(back_populates='user')
+    comments: Mapped[list['Comment']] = relationship(back_populates='author_comment')
+    follower: Mapped[list['Follower']] = relationship(back_populates='follower_user')
+    following: Mapped[list['Follower']] = relationship(back_populates='following_user')
 
+class Post(db.Model):
+    __tablename__ = 'post'
+    post_id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey('user.user_id'))
+    user: Mapped['User'] = relationship(back_populates='posts')
+    media: Mapped[list['Media']] = relationship(back_populates='post')
+    comments: Mapped[list['Comment']] = relationship(back_populates='post')
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+class Comment(db.Model):
+    __tablename__ = 'comment'
+    comment_id: Mapped[int] = mapped_column(primary_key=True)
+    comment_text: Mapped[str] = mapped_column(String(100))
+    author_id: Mapped[str] = mapped_column(ForeignKey('user.user_id'))
+    author_comment: Mapped['User'] = relationship(back_populates='comments')
+    post_id: Mapped[int] = mapped_column(ForeignKey('post.post_id'))
+    post: Mapped['Post'] = relationship(back_populates='comments')
+
+class Media(db.Model):
+    __tablename__ = 'media'
+    media_id: Mapped[int] = mapped_column(primary_key=True)
+    url: Mapped[str] = mapped_column(String(100))
+    post_id: Mapped[int] = mapped_column(ForeignKey('post.post_id'))
+    post: Mapped['Post'] = relationship(back_populates='media')
+
+class Follower(db.Model):
+    __tablename__ = 'follower'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_from_id: Mapped[int] = mapped_column(ForeignKey('user.user_id'))
+    follower_user: Mapped['User'] = relationship(back_populates='follower')
+    user_to_id: Mapped[int] = mapped_column(ForeignKey('user.user_id'))
+    following_user: Mapped['User'] = relationship(back_populates='following')
